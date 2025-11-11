96662b5cec1d66ed6590b6fdbe27a0bd4dfd3f81
Ttibsi <ashisbitt@icloud.com>
2025-11-11 23:18:56 +0000

    more simple values in elf header

---
+++ src/header.d
@@ -0,0 +1,53 @@
+import utils;
+import std.bitmanip;
+
+byte[16] e_ident() {
+    byte[16] eident = new byte[16];
+
+    // first 4 bytes are a magic number, default
+    eident[0] = 0x7f;
+    eident[1] = 0x45; // 'e'
+    eident[2] = 0x4c; // 'l'
+    eident[3] = 0x46; // 'f'
+
+    // 32 or 64 bit file
+    eident[4] = 0x02;
+
+    // LSB or MSB
+    eident[5] = 0x01;
+
+    // Version (alway 1)
+    eident[6] = 0x01;
+
+    // OS ABI
+    // usually 0/System V even on linux, but some static libs set this to 3/linux
+    // for now, we will keep this at 0
+    eident[7] = 0x00;
+
+    // ABI version (almost never used)
+    eident[8] = 0x00;
+
+    // the rest of the bytes are for padding
+
+    return eident;
+}
+
+// NOTE: 2 is for executables that don't support position-independant code
+// use 3 (for SO) for an executable that uses PIE
+byte[] e_type() pure { return [2, 0]; }
+
+byte[] elf_header() {
+    byte[] header = [];
+    header ~= e_ident();
+
+    // file type 
+    header ~= e_type();
+
+    // architecture
+    header ~= nativeToLittleEndian(getCurrArch());
+
+    // Version -- always 1
+    header ~= [1,0,0,0];
+
+    return header;
+}
--- src/utils.d
+++ src/utils.d
@@ -1,19 +1,23 @@
 import std.system;
 import std.stdio;
+import std.conv;
 
-byte getCurrArch() {
+// type aliases so it's easier to read
+alias uint16_t = ushort;
+alias uint32_t = uint;
+alias uint64_t = ulong;
+
+ushort getCurrArch() {
     switch (instructionSetArchitecture) {
         case ISA.aarch64:
-            return 1;
+            return 0xB7;
         case ISA.x86_64:
-            return 2;
+            return 0x3E;
         default:
-            return 127;
+            return 0;
     }
 }
 
 void writeToFile(byte[] bytes) {
     toFile(bytes, "t");
 }
-
-void hello() { writeln("Hello world"); }
